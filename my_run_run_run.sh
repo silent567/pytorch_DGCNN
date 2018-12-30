dcee5511858c08ff8b8172a301895739fec82417
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-30 23:43:16 +0900

    Config my_run_run_run.sh

--- my_run_run_run.sh
+++ my_run_run_run.sh
@@ -7,8 +7,8 @@ norm_flag="${3-True}" #layer_norm_flag for attention
 gamma=${4-1.0} #gamma controlling the sparsity, the smaller the sparser 
 lam=${5-1.0} #lambda controlling the smoothness, the larger the smoother
 
-./my_run_run.sh $gm 5 $max_type $norm_flag $gamma $lam 0 2
-./my_run_run.sh $gm 5 $max_type $norm_flag $gamma $lam 2 2
-./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 4 2
-./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 6 2
-./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 8 2
+./my_run_run.sh $gm 5 $max_type $norm_flag $gamma $lam 0 2 &
+./my_run_run.sh $gm 5 $max_type $norm_flag $gamma $lam 2 2 &
+./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 4 2 &
+./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 6 2 &
+./my_run_run.sh $gm 6 $max_type $norm_flag $gamma $lam 8 2 &
