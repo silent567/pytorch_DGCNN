7b91b8b9c45c901bc3621948d39102b058411b2e
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-30 14:27:18 +0900

    Add torch_mapping and torch_attention from SEED_emotion

---
+++ torch_attention.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from torch_mapping import torch_sparsemax, Gfusedmax
+import torch
+import numpy as np
+
+def build_graph(kernel_size):
+    size = kernel_size *kernel_size
+    output = np.zeros([size,size])
+    for i in range(kernel_size):
+        for j in range(i,kernel_size):
+            start_index = i*kernel_size+j
+            if i > 0:
+                output[start_index,start_index-kernel_size] = 1
+                output[start_index-kernel_size,start_index] = 1
+            if i < kernel_size-1:
+                output[start_index,start_index+kernel_size] = 1
+                output[start_index+kernel_size,start_index] = 1
+            if j > 0:
+                output[start_index,start_index-1] = 1
+                output[start_index-1,start_index] = 1
+            if j < kernel_size-1:
+                output[start_index,start_index+1] = 1
+                output[start_index+1,start_index] = 1
+    return output
+
+class AddAttention(torch.nn.Module):
+    def __init__(self,input_size,output_size,channel_num,A,max_type='softmax',layer_norm_flag=True,lam=1.0,gamma=1.0,query_size=0):
+        super(AddAttention,self).__init__()
+        if max_type == 'sparsemax':
+            if gamma is None:
+                self.register_parameter('gamma',torch.nn.Parameter(torch.ones([],dtype=torch.float,requires_grad=True)))
+            else:
+                self.gamma = gamma
+            self.mapping_func = lambda x,dim: torch_sparsemax.apply(x,dim,self.gamma)
+        elif max_type == 'gfusedmax':
+            self.gamma = gamma if gamma is not None else 1.0
+            self.lam = lam if lam is not None else 1.0
+            self.register_buffer('input_A',torch.from_numpy(A).unsqueeze_(0).unsqueeze_(-1))
+            self.gfusedmax_module = Gfusedmax(self.gamma,self.lam)
+            self.mapping_func = lambda x,dim: self.gfusedmax_module(x,self.input_A,dim)
+        elif max_type == 'mean':
+            self.mapping_func = lambda x,dim: torch.ones_like(x)/x.size()[dim]
+        elif max_type == 'sum':
+            self.mapping_func = lambda x,dim: torch.ones_like(x)
+        else:
+            self.mapping_func = torch.nn.functional.softmax
+
+        self.output_size = output_size
+        self.channel_num = channel_num
+        self.query_size = query_size
+
+        self.proj_func = torch.nn.Linear(input_size,output_size)
+        self.score_func = torch.nn.Linear(input_size+query_size,1)
+        if layer_norm_flag:
+            self.score_norm = torch.nn.LayerNorm([self.channel_num,1],elementwise_affine=channel_num is not None)
+        else:
+            self.score_norm = lambda x:x
+    def forward(self,x,q=None):
+        '''
+        x's shape = [N,M,C]
+        q's shape = [N,C']
+        return y's shape = [N,C'']
+        '''
+        N,M,C = x.size()
+
+        proj_x = self.proj_func(x) #[N,M,C'']
+        score_x = self.score_func(x if self.query_size < 1 else torch.cat(
+            [x,q.unsqueeze_(1).expand(-1,M,-1)],dim=-1)) #[N,M,1]
+        weights = self.mapping_func(self.score_norm(score_x),dim=-2)
+        output = torch.sum(proj_x * weights,dim=-2)
+
+        return output
+
+class ConvAddAttention(torch.nn.Module):
+    def __init__(self,input_size,output_size,kernel_size,stride_size,max_type='softmax',layer_norm_flag=True,lam=1.0,gamma=1.0,query_size=0):
+        super(ConvAddAttention,self).__init__()
+        if max_type == 'sparsemax':
+            if gamma is None:
+                self.register_parameter('gamma',torch.nn.Parameter(torch.ones([],dtype=torch.float,requires_grad=True)))
+            else:
+                self.gamma = gamma
+            self.mapping_func = lambda x,dim: torch_sparsemax.apply(x,dim,self.gamma)
+        elif max_type == 'gfusedmax':
+            self.gamma = gamma if gamma is not None else 1.0
+            self.lam = lam if lam is not None else 1.0
+            self.register_buffer('input_A',torch.from_numpy(build_graph(kernel_size)).unsqueeze_(0).unsqueeze_(-1))
+            self.gfusedmax_module = Gfusedmax(self.gamma,self.lam)
+            self.mapping_func = lambda x,dim: self.gfusedmax_module(x,self.input_A,dim)
+        else:
+            self.mapping_func = torch.nn.functional.softmax
+
+        self.output_size = output_size
+        self.kernel_size = kernel_size
+        self.stride_size = stride_size
+        self.query_size = query_size
+
+        self.proj_func = torch.nn.Linear(input_size,output_size)
+        self.score_func = torch.nn.Linear(input_size+query_size,1)
+        if layer_norm_flag:
+            self.score_norm = torch.nn.LayerNorm([self.kernel_size*self.kernel_size,1])
+        else:
+            self.score_norm = lambda x:x
+        # self.proj_func.to('cuda')
+        # self.score_func.to('cuda')
+    def forward(self,x,q=None):
+        '''
+        x's shape = [N,C,H,W]
+        q's shape = [N,C']
+        '''
+        N,C,H,W = x.size()
+
+        # x = x.reshape([N,H,W,C])
+        x = torch.transpose(x,1,3) #[N,W,H,C]
+        proj_x = self.proj_func(x) #[N,W,H,C']
+        score_x = self.score_func(x if self.query_size < 1 else torch.cat([x,q.unsqueeze_(1).unsqueeze_(1).expand(-1,W,H,-1)],dim=-1)) #[N,W,H,1]
+        output = []
+        for h in range(0,H-self.kernel_size+1,self.stride_size):
+            tmp_output = []
+            for w in range(0,W-self.kernel_size+1,self.stride_size):
+                scores = torch.reshape(score_x[:,w:w+self.kernel_size,h:h+self.kernel_size,:],[N,-1,1]) #[N,k*k,1]
+                projs = torch.reshape(proj_x[:,w:w+self.kernel_size,h:h+self.kernel_size,:],[N,-1,self.output_size]) #[N,k*k,C']
+                weights = self.mapping_func(self.score_norm(scores),dim=-2) #[N,k*k,1]
+                # print(scores.size(),weights.size())
+                tmp_output.append(torch.sum(projs * weights,dim=-2)) #[N,C']
+            output.append(torch.stack(tmp_output,dim=-1)) #[N,C',W]
+        output = torch.stack(output,dim=-2) #[N,C',H,W]
+
+        return output
+    def to(self,device):
+        super(ConvAddAttention,self).to(device)
+        self.gamma = self.gamma.to(device)
+
+def l2_norm(x,dim):
+    x = (x - torch.mean(x,dim=dim,keepdim=True))/torch.std(x,dim=dim,keepdim=True)
+    return x
+
+
+class FlexAddAttention(torch.nn.Module):
+    def __init__(self,input_size,output_size,channel_num,max_type='softmax',layer_norm_flag=True,lam=1.0,gamma=1.0,query_size=0):
+        super(FlexAddAttention,self).__init__()
+        if max_type == 'sparsemax':
+            if gamma is None:
+                self.register_parameter('gamma',torch.nn.Parameter(torch.ones([],dtype=torch.float,requires_grad=True)))
+            else:
+                self.gamma = gamma
+            self.mapping_func = lambda x,dim,A: torch_sparsemax.apply(x,dim,self.gamma)
+        elif max_type == 'gfusedmax':
+            self.gamma = gamma if gamma is not None else 1.0
+            self.lam = lam if lam is not None else 1.0
+            self.gfusedmax_module = Gfusedmax(self.gamma,self.lam)
+            self.mapping_func = lambda x,dim,A: self.gfusedmax_module(x,A,dim)
+        elif max_type == 'mean':
+            self.mapping_func = lambda x,dim,A: torch.ones_like(x)/x.size()[dim]
+        elif max_type == 'sum':
+            self.mapping_func = lambda x,dim,A: torch.ones_like(x)
+        else:
+            self.mapping_func = lambda x,dim,A:torch.nn.functional.softmax(x,dim=dim)
+
+        self.output_size = output_size
+        self.channel_num = channel_num
+        self.query_size = query_size
+
+        self.proj_func = torch.nn.Linear(input_size,output_size)
+        self.score_func = torch.nn.Linear(input_size+query_size,1)
+        if layer_norm_flag:
+            if channel_num is not None:
+                self.score_norm = torch.nn.LayerNorm([self.channel_num,1])
+            else:
+                self.score_norm = lambda x:l2_norm(x,-2)
+        else:
+            self.score_norm = lambda x:x
+    def forward(self,x,A,q=None):
+        '''
+        x's shape = [N,M,C]
+        A's shape = [N,M,M,1]
+        q's shape = [N,C']
+        return y's shape = [N,C'']
+        '''
+        # print(x.size(),A.size(),q.size())
+        N,M,C = x.size()
+
+        proj_x = self.proj_func(x) #[N,M,C'']
+        if x.size()[1] < 2:
+            return proj_x[:,0]
+        score_x = self.score_func(x if self.query_size < 1 else torch.cat(
+            [x,q.unsqueeze_(1).expand(-1,M,-1)],dim=-1)) #[N,M,1]
+        weights = self.mapping_func(self.score_norm(score_x),-2,A)
+        output = torch.sum(proj_x * weights,dim=-2)
+
+        return output
+
+class NodewiseAttention(torch.nn.Module):
+    def __init__(self,input_size,output_size,channel_num,A,max_type='softmax',layer_norm_flag=True,lam=1.0,gamma=1.0,query_size=0):
+        super(NodewiseAttention,self).__init__()
+        self.query_size = query_size
+        self.register_buffer('input_A',torch.from_numpy(A).unsqueeze_(-1).unsqueeze_(0)) #[1,M,M,1]
+        self.register_buffer('input_A_flag',torch.from_numpy(A+np.eye(A.shape[0]))>0) #[M,M]
+        self.aggr = FlexAddAttention(input_size+query_size,output_size,channel_num,max_type,layer_norm_flag,lam,gamma,input_size)
+    def forward(self,x,q=None):
+        '''
+        x's shape = [N,M,C]
+        q's shape = [N,C']
+        return y's shape = [N,M,C'']
+        '''
+        x_and_q = torch.cat([x,q.unsqueeze_(1).expand([-1,x.size()[1],-1])],dim=-1) if self.query_size > 0 else x
+        input_A = self.input_A.expand([x.size()[0],-1,-1,-1])
+        output = []
+        for n in range(x.size()[1]):
+            # print(x_and_q[:,self.input_A_flag[n]].size())
+            # print(input_A.size())
+            # print(input_A[:,self.input_A_flag[n],:,:].size())
+            # print(input_A[:,self.input_A_flag[n],:,:][:,:,self.input_A_flag[n],:].size())
+            # print(x[:,n].size())
+            output.append(self.aggr(x_and_q[:,self.input_A_flag[n]]
+                                    ,input_A[:,self.input_A_flag[n],:,:][:,:,self.input_A_flag[n],:]
+                                    ,x[:,n])) #[N,C'']
+        output = torch.stack(output,dim=-2)
+        return output
+
+class GraphAddAttention(torch.nn.Module):
+    def __init__(self,input_size,output_size,channel_num,A,max_type='softmax',layer_norm_flag=True,lam=1.0,gamma=1.0,query_size=0):
+        super(GraphAddAttention,self).__init__()
+        if max_type == 'sparsemax':
+            if gamma is None:
+                self.register_parameter('gamma',torch.nn.Parameter(torch.ones([],dtype=torch.float,requires_grad=True)))
+            else:
+                self.gamma = gamma
+            self.mapping_func = lambda x,dim: torch_sparsemax.apply(x,dim,self.gamma)
+        elif max_type == 'gfusedmax':
+            self.gamma = gamma if gamma is not None else 1.0
+            self.lam = lam if lam is not None else 1.0
+            self.register_buffer('input_A',torch.from_numpy(A).unsqueeze_(0).unsqueeze_(-1))
+            self.gfusedmax_module = Gfusedmax(self.gamma,self.lam)
+            self.mapping_func = lambda x,dim: self.gfusedmax_module(x,self.input_A,dim)
+        elif max_type == 'mean':
+            self.mapping_func = lambda x,dim: torch.ones_like(x)/x.size()[dim]
+        elif max_type == 'sum':
+            self.mapping_func = lambda x,dim: torch.ones_like(x)
+        else:
+            self.mapping_func = torch.nn.functional.softmax
+
+        self.output_size = output_size
+        self.channel_num = channel_num
+        self.query_size = query_size
+
+        self.proj_att = NodewiseAttention(input_size,output_size,None,A,max_type,layer_norm_flag,lam,gamma,query_size)
+        self.score_att = NodewiseAttention(input_size,1,None,A,max_type,layer_norm_flag,lam,gamma,query_size)
+        if layer_norm_flag:
+            self.score_norm = torch.nn.LayerNorm([self.channel_num,1])
+        else:
+            self.score_norm = lambda x:x
+    def forward(self,x,q=None):
+        '''
+        x's shape = [N,M,C]
+        q's shape = [N,C']
+        return y's shape = [N,C'']
+        '''
+        N,M,C = x.size()
+
+        proj_x = self.proj_att(x) #[N,M,C'']
+        score_x = self.score_att(x) #[N,M,1]
+        weights = self.mapping_func(self.score_norm(score_x),dim=-2)
+        output = torch.sum(proj_x * weights,dim=-2)
+
+        return output
+
+
+
+
---
+++ torch_mapping.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import torch
+from mapping import sparsemax, gfusedlasso, gfusedmax
+
+class torch_sparsemax(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inp, dim=-1, gamma=1.0):
+        if not torch.is_tensor(gamma):
+            gamma = torch.tensor(gamma,device=inp.device,dtype=inp.dtype)
+
+        reshape_size = [1]*len(inp.size())
+        reshape_size[dim] = -1
+
+        inp_div = inp / gamma
+        inp_sorted,_ = torch.sort(inp_div, dim=dim, descending=True)
+        cumsum = torch.cumsum(inp_sorted,dim=dim)
+        mask = (1+torch.arange(1,inp_div.size()[dim]+1,device=inp.device,dtype=inp.dtype)
+                .reshape(reshape_size)*inp_sorted) > cumsum
+        mask = mask.type_as(inp)
+        tau = (torch.sum(inp_sorted*mask,dim=dim,keepdim=True)-1.)/torch.sum(mask,dim=dim,keepdim=True,dtype=inp.dtype)
+        output = torch.clamp(inp_div-tau,min=0)
+
+        ctx.dim = dim
+        ctx.save_for_backward(inp, gamma, output)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        inp, gamma, output = ctx.saved_tensors
+        dim = ctx.dim
+        # print(ctx.needs_input_grad,gamma.device)
+
+        mask = (output > 0).type_as(inp)
+        masked_grad_output = grad_output*mask
+        # print('masked_grad_out:',masked_grad_output.size(),masked_grad_output.dtype,masked_grad_output.device,masked_grad_output.norm().item(),masked_grad_output.std().item())
+        # mask_sum = torch.sum(mask,dim=dim,keepdim=True)
+        # print('mask_sum:       ',mask_sum.size(),mask_sum.dtype,mask_sum.device,mask_sum.norm().item(),mask_sum.std().item())
+        masked_grad_output -= mask * (torch.sum(masked_grad_output,dim=dim,keepdim=True)\
+                            / (torch.sum(mask,dim=dim,keepdim=True)+1e-5))
+
+        grad_inp = None
+        if ctx.needs_input_grad[0]:
+            grad_inp = masked_grad_output / gamma
+        if len(ctx.needs_input_grad) < 2:
+            return grad_inp
+
+        if ctx.needs_input_grad[1]:
+            raise ValueError('No gradient is defined for dim argument of sparsemax')
+        if len(ctx.needs_input_grad) < 3:
+            return grad_inp,None
+
+        grad_gamma = None
+        if ctx.needs_input_grad[2]:
+            grad_gamma = -torch.sum(masked_grad_output*inp*mask)/gamma/gamma
+        # print('inp:            ',inp.size(),inp.dtype,inp.device,inp.norm().item(),inp.std().item())
+        # print('output:         ',output.size(),output.dtype,output.device,output.norm().item(),output.std().item())
+        # print('mask:           ',mask.size(),mask.dtype,mask.device,mask.norm().item(),mask.std().item())
+        # print('masked_grad_out:',masked_grad_output.size(),masked_grad_output.dtype,masked_grad_output.device,masked_grad_output.norm().item(),masked_grad_output.std().item())
+        # print('grad_out:       ',grad_output.size(),grad_output.dtype,grad_output.device,grad_output.norm().item(),grad_output.std().item())
+        # print('grad_inp:       ',grad_inp.size(),grad_inp.dtype,grad_inp.device,grad_inp.norm().item(),grad_inp.std().item())
+        # if input() == 'exit':
+            # raise ValueError('Commanded to exit')
+        return grad_inp, None, grad_gamma
+
+def backward_gfusedmax_torch_1D(output, grad_output):
+    '''
+    input's shape = [d]
+    A's shape = [d,d]
+    lam's shape = []
+    output's shape = [d]
+    grad_output's shape = [d]
+
+    return grad_input's shape = [d]
+    '''
+    grad_input = torch.zeros_like(grad_output)
+    unique_output = torch.unique(output)
+    for uo in unique_output.unbind():
+        mask = output == uo
+        grad_input[mask] = torch.sum(grad_output[mask])/torch.sum(mask)
+
+    ##### A and lam's gradients to be implemented
+    return grad_input
+
+class torch_gfusedlasso(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inp, A, dim=-1, lam=1.0):
+        '''
+        inp's shape = [*,M,*]
+        A's shape = [*,M,M,*]
+        '''
+        if not torch.is_tensor(lam):
+            lam = torch.zeros([],device=inp.device,dtype=inp.dtype) + lam
+        if A.size()[0] == 1:
+            A = A.expand([inp.size()[0],]+[-1]*(len(A.size())-1))
+
+        M = inp.size()[dim]
+        inp_reshape_size = list(inp.size())
+        inp_reshape_size[dim],inp_reshape_size[-1] = inp_reshape_size[-1],inp_reshape_size[dim]
+
+        inp_reshape = torch.reshape(torch.transpose(inp,dim,-1),[-1,M])
+        A_reshape = torch.reshape(torch.transpose(torch.transpose(A,dim+1,-1),dim,-2),[-1,M,M])
+
+        # print(type(lam.item()),lam.item())
+        cpu_detach = lambda x: x.cpu().detach_() if x.is_cuda else x.detach()
+        cuda_back = lambda x: x.cuda() if inp.is_cuda else x
+        output_reshape = torch.stack([torch.from_numpy(gfusedlasso(i.numpy(),a.numpy(),lam=lam.item()))
+                                      for i,a in zip(cpu_detach(inp_reshape).unbind(),cpu_detach(A_reshape).unbind())],dim=0)
+        output = torch.transpose(torch.reshape(cuda_back(output_reshape),inp_reshape_size),dim,-1)
+
+        ctx.dim, ctx.M, ctx.inp_reshape_size = dim, M, inp_reshape_size
+        ctx.save_for_backward(output_reshape)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if len(ctx.needs_input_grad) < 1 or not ctx.needs_input_grad[0]:
+            raise ValueError('Only gradients for x in the gfusedlasso is implemented')
+        if len(ctx.needs_input_grad) > 1 and ctx.needs_input_grad[1]:
+            raise ValueError('Only gradients for x in the gfusedlasso is implemented')
+        if len(ctx.needs_input_grad) > 2 and ctx.needs_input_grad[2]:
+            raise ValueError('Only gradients for x in the gfusedlasso is implemented')
+        if len(ctx.needs_input_grad) > 3 and ctx.needs_input_grad[3]:
+            raise ValueError('Only gradients for x in the gfusedlasso is implemented')
+
+        output_reshape, = ctx.saved_tensors
+        dim, M, inp_reshape_size  = ctx.dim, ctx.M, ctx.inp_reshape_size
+
+        grad_output_reshape = torch.reshape(torch.transpose(grad_output,dim,-1),[-1,M])
+        grad_inp_reshape = torch.stack([backward_gfusedmax_torch_1D(o,go) for o,go in zip(
+            output_reshape.unbind(), grad_output_reshape.unbind()
+        )],dim=0)
+        grad_inp = torch.transpose(torch.reshape(grad_inp_reshape,inp_reshape_size),dim,-1)
+
+        return (grad_inp,)+(None,)*(len(ctx.needs_input_grad)-1)
+
+class Gfusedmax(torch.nn.Module):
+    def __init__(self,gamma=1.0,lam=1.0):
+        super(Gfusedmax,self).__init__()
+        self.gamma = gamma
+        self.gfusedlasso_func = lambda x,A,dim: torch_gfusedlasso.apply(x,A,dim,lam)
+        self.sparsemax_func = lambda x,dim: torch_sparsemax.apply(x,dim)
+    def forward(self,x,A,dim=-1):
+        x = x / self.gamma
+        fused_x = self.gfusedlasso_func(x,A,dim)
+        output = self.sparsemax_func(fused_x,dim)
+        return output
+
+if __name__ == '__main__':
+    size = 10
+    import numpy as np
+    numpy_a = np.array([ 0.1761,  0.1761,  0.1761,  0.1761,  0.1761,  0.1761,  0.1761,  0.1761, 0.9138,  0.1761,  0.1761,  0.1761,  1.9040, -0.7119,  0.1761,  0.1761])
+    a = torch.tensor(numpy_a,requires_grad=True,dtype=torch.float)
+    # a = torch.rand(size,requires_grad=True)
+    lam = torch.tensor(10.0,requires_grad=True,dtype=torch.float)
+    torch_sparse = torch_sparsemax.apply(a,-1,lam)
+    numpy_sparse = sparsemax(a.detach().numpy(),lam.item())
+    torch_sparse.backward(torch.arange(a.size()[-1],dtype=a.dtype))
+    print(a,torch.sum(torch_sparse),np.sum(numpy_sparse))
+    print(a.grad, lam.grad)
+
+    # b = a * 30
+    # A = (torch.rand(size,size)>0.9).type_as(a)
+    # lam = lam.detach()
+    # torch_gfusedmax = Gfusedmax(lam,lam)(b,A,-1)
+    # numpy_gfusedmax = gfusedmax(b.detach().numpy(),A.numpy(),lam.item(),lam.item())
+    # torch_gfusedmax.backward(torch.arange(size,dtype=a.dtype))
+    # print(b,torch_gfusedmax,numpy_gfusedmax)
+    # print(a.grad,)
