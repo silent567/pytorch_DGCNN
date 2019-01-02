77c5c3d0827f7e4ad1aa469fa532ddd0e3c82d0d
BlluServer2 <silent56@sjtu.edu.cn>
2019-01-02 19:29:13 +0800

    Config stop.sh to python3

--- stop.sh
+++ stop.sh
@@ -2,6 +2,6 @@
 
 while (sleep 1);
 do
-    ps -aux | grep python | grep -v python3 | grep $1 | awk '{print $2}' | xargs kill;
+    ps -aux | grep python3 | grep $1 | awk '{print $2}' | xargs kill;
 done
 
