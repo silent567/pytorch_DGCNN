13e678c837800f75e6167aa7d1cd9c3aee66a29b
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2019-01-03 14:11:25 +0900

    Start another tune on teodora

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 5 3 &
-./my_run2.sh AttPool 5 5 3 &
-#./my_run2.sh AttPool 6 4 "" &
+./my_run2.sh AttPool 0 3 6 &
+./my_run2.sh AttPool 3 3 6 &
+./my_run2.sh AttPool 6 4 6 &
