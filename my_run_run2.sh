706717c1b5ef654a072edc0f1771eb3299ddf60f
BlluServer2 <silent56@sjtu.edu.cn>
2019-01-04 15:21:37 +0800

    Add results

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 3 6 &
-./my_run2.sh AttPool 3 3 6 &
-./my_run2.sh AttPool 6 4 6 &
+./my_run2.sh AttPool 0 3 0 &
+./my_run2.sh AttPool 3 3 0 &
+./my_run2.sh AttPool 6 4 0 &
