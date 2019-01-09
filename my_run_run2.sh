573e9c6f710a87666f956ecac56178ebb0474888
BlluServer2 <silent56@sjtu.edu.cn>
2019-01-09 22:04:48 +0800

    Start to tune on bllu_server2

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 3 0 &
-./my_run2.sh AttPool 3 3 0 &
-./my_run2.sh AttPool 6 4 0 &
+./my_run2.sh AttPool 0 1 0 &
+./my_run2.sh AttPool 1 1 0 &
+./my_run2.sh AttPool 2 1 0 &
+./my_run2.sh AttPool 3 1 1 &
+./my_run2.sh AttPool 4 1 1 &
+./my_run2.sh AttPool 5 1 1 &
+./my_run2.sh AttPool 6 1 2 &
+./my_run2.sh AttPool 7 1 2 &
+./my_run2.sh AttPool 8 1 2 &
+./my_run2.sh AttPool 9 1 "" &
