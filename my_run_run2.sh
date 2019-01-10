19207b4d7af4a5a6c40096db57f6a17f9ed8f94f
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2019-01-10 11:01:16 +0900

    Start to tune on nero after debugging gfusedlasso

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 1 4 &
-./my_run2.sh AttPool 1 1 4 &
-./my_run2.sh AttPool 2 1 4 &
-./my_run2.sh AttPool 3 1 5 &
-./my_run2.sh AttPool 4 1 5 &
-./my_run2.sh AttPool 5 1 5 &
-./my_run2.sh AttPool 6 1 6 &
-./my_run2.sh AttPool 7 1 6 &
-./my_run2.sh AttPool 8 1 6 &
-./my_run2.sh AttPool 9 1 "" &
+./my_run2.sh AttPool 0 2 3 &
+./my_run2.sh AttPool 2 2 3 &
+./my_run2.sh AttPool 4 2 "" &
+./my_run2.sh AttPool 6 2 "" &
+./my_run2.sh AttPool 8 2 "" &
