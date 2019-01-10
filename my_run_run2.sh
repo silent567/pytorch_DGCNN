d16bc065e281de7df305107f3bc12565062793f9
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2019-01-10 10:28:28 +0900

    Start to tune on teodora after debugging gfusedlasso

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 1 0 &
-./my_run2.sh AttPool 1 1 0 &
-./my_run2.sh AttPool 2 1 0 &
-./my_run2.sh AttPool 3 1 1 &
-./my_run2.sh AttPool 4 1 1 &
-./my_run2.sh AttPool 5 1 1 &
-./my_run2.sh AttPool 6 1 2 &
-./my_run2.sh AttPool 7 1 2 &
-./my_run2.sh AttPool 8 1 2 &
+./my_run2.sh AttPool 0 1 4 &
+./my_run2.sh AttPool 1 1 4 &
+./my_run2.sh AttPool 2 1 4 &
+./my_run2.sh AttPool 3 1 5 &
+./my_run2.sh AttPool 4 1 5 &
+./my_run2.sh AttPool 5 1 5 &
+./my_run2.sh AttPool 6 1 6 &
+./my_run2.sh AttPool 7 1 6 &
+./my_run2.sh AttPool 8 1 6 &
 ./my_run2.sh AttPool 9 1 "" &
