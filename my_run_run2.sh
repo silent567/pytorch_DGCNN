03263c86544865c658ad790c97b24e5a206c4cc5
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2019-01-13 20:33:35 +0900

    Start to tune on teodora & nero

--- my_run_run2.sh
+++ my_run_run2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-./my_run2.sh AttPool 0 2 3 &
-./my_run2.sh AttPool 2 2 3 &
-./my_run2.sh AttPool 4 2 "" &
-./my_run2.sh AttPool 6 2 "" &
-./my_run2.sh AttPool 8 2 "" &
+#./my_run2.sh AttPool 0 1 4 &
+#./my_run2.sh AttPool 1 1 4 &
+#./my_run2.sh AttPool 2 1 4 &
+#./my_run2.sh AttPool 3 1 5 &
+#./my_run2.sh AttPool 4 1 5 &
+#./my_run2.sh AttPool 5 1 5 &
+#./my_run2.sh AttPool 6 1 6 &
+./my_run2.sh AttPool 0 2 6 &
+./my_run2.sh AttPool 2 2 6 &
+#./my_run2.sh AttPool 9 1 "" &
+./my_run2.sh AttPool 4 2 7 &
+./my_run2.sh AttPool 6 2 7 &
+./my_run2.sh AttPool 8 2 7 &
