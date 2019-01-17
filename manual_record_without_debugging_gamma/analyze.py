1c404cf4a3f195b1d0f5d5db3ce4a456e4bc6c09
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2019-01-17 13:32:40 +0900

    Move manual record to manual_record_without_debugging gamma

--- manual_record_without_debugging_gamma/analyze.py
+++ manual_record_without_debugging_gamma/analyze.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[4]:
+# In[5]:
 
 
 dataset_list = ['MUTAG','PTC','PROTEINS','NCI1','NCI109','COLLAB','IMDBBINARY','IMDBMULTI','DD','ENZYMES']
@@ -25,4 +25,22 @@ for i,dataset_name in enumerate(dataset_list):
         print(records[np.argmax(records[:,-1])])
     except:
         pass
+
+
+# In[7]:
+
+
+dataset_list = ['MUTAG','PTC','PROTEINS','NCI1','NCI109','COLLAB','IMDBBINARY','IMDBMULTI','DD','ENZYMES']
+dataset_name = dataset_list[1]
+print(dataset_name)
+model_name = 'FastMultiAttPool'
+filenames = filter(lambda fn:dataset_name in fn.split('_') and model_name in fn.split('_'),os.listdir())
+records = np.concatenate([np.loadtxt(fn,delimiter=',') for fn in filenames],axis=0)
+records = records[records[:,-1]>0]
+
+print(records[:,0])
+
+print(filenames)
+print(records.shape)
+print(records[np.argmax(records[:,-1])])
 
